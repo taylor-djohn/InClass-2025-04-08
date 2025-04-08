de4277a5d60a797af0b47cf95c684a875ef60f30
Jenna Liette <lietteja@mail.uc.edu>
2025-04-08 12:17:19 -0400

    Added calculation for the click event when choosing dates of stay on calendar.

--- InClass 2025-04-08/index.aspx.cs
+++ InClass 2025-04-08/index.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +12,31 @@ namespace InClass_2025_04_08
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            cmdOk.Click += cmdOk_Click;
+        }
 
+        protected void cmdOk_Click(object sender, EventArgs e)
+        {
+            DateTime startDate = cldCheckInDate.SelectedDate;
+            DateTime endDate = cldCheckOutDate.SelectedDate;
+            double perDayRate = 200;
+            double salesTax = 0.15;
+
+            // Ensure both dates are selected
+            if (startDate == DateTime.MinValue || endDate == DateTime.MinValue)
+            {
+                lblResult.Text = "Please select both dates.";
+                return;
+            }
+
+            // Calculate the difference in days
+            TimeSpan dateDifference = endDate - startDate;
+            int numberOfDays = dateDifference.Days;
+            double totalCost = numberOfDays * perDayRate * (salesTax + 1);
+
+            // Display the result
+            lblResult.Text = "Your total cost for the stay is $" + totalCost;
+           
         }
     }
 }
